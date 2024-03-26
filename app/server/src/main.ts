eb27982006154454e6fd7e496fa14c86543c4996
FRONTfikry <edvard2027@gmail.com>
2024-03-26 22:03:44 +0300

    init(server)

---
+++ app/server/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from 'app/app.module'
+
+
+async function bootstrap() {
+  const PORT = process.env.PORT
+
+  const app = await NestFactory.create(AppModule)
+
+  await app.listen(PORT)
+}
+
+
+bootstrap()
